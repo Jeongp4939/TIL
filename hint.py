235d72fd39acc6576e297a6c41904d09f9bf3746
jeong <jeongp12@gmail.com>
2023-01-19 22:15:33 +0900

    home commit 2

--- hint.py
+++ hint.py
@@ -1,12 +1,19 @@
 def calcul_score(score):
-
     total = 0
     cnt=0
     for cost in score.values():
         total += cost
         cnt+=1
+    print(total)
     return total/cnt
 
+def max_min(score):
+    score_lst = list(score.values())
+    for i in range(len(score_lst)):
+        for j in range(i,len(score_lst)):
+            if score_lst[i] > score_lst[j]:
+                score_lst[i],score_lst[j] = score_lst[j],score_lst[i]
+    return (score_lst[-1],score_lst[0])
 
 if __name__ == '__main__':
     score = {
@@ -16,8 +23,15 @@ if __name__ == '__main__':
         '허수아비' : 290,
         '온유' : 180, 
     }
+    
+    print(calcul_score(score))
+    print(max_min(score))
+    for i in score.items():
+        if i[1]==max_min(score)[1]:
+            print(len(i[0]))
 
-    print(calcul_score(score))  
+    
+
     
     # 점수들의 합 출력 하시오       답: 1680
     # 점수들의 평균을 출력 하시오   답: 336 또는 336.0
