f05068ca00a18d90c8eef8b7e9b8d6c4fdb65412
Jeong4939 <jeongp12@gmail.com>
2023-05-02 11:10:59 +0900

    230502 commit

---
+++ vue_js/vue-cli/src/components/MyChild.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>This is Child component</h3>
+    <a href="">건드리면 물어요</a>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MyChild'
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_js/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>This is MyComponent</h1>
+    <MyChild msg="HaHaHaHa"/>
+    <MyChild msg="HaHaHaHa"/>
+    <MyChild msg="HaHaHaHa"/>
+    <MyChild msg="HaHaHaHa"/>
+  </div>
+</template>
+
+<script>
+import MyChild from '@/components/MyChild.vue';
+
+export default {
+    name: 'MyComponent',
+    components: {
+        MyChild,
+    }
+}
+</script>
+
+<style>
+
+</style>
